48ddab6d9085f8874d2af68b8190192a8e378368
Roman Yezerskyi <yezerskyi.roman@gmail.com>
2022-07-24 09:52:53 +0300

    Added power generator

---
+++ CampTask2/PowerGenerator/SuperPowerGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CampTask2.PowerGenerator
+{
+    public class SuperPowerGenerator
+    {
+        public int GetSuperPower()
+        {
+            var date = DateTime.Now.Second;
+            while (true)
+            {
+                if (date >= 5 && date <= 10)
+                {
+                    return date;
+                }
+
+                if (date < 5)
+                {
+                    return date + 5;
+                }
+
+                if (date > 10)
+                {
+                    date /= 10;
+                }
+            }
+        }
+    }
+}
