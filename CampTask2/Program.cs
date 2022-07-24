d9fea0ba2dd070cd560d402edee8153241b4bf4a
Roman Yezerskyi <yezerskyi.roman@gmail.com>
2022-07-24 10:16:13 +0300

    final

--- CampTask2/Program.cs
+++ CampTask2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using CampTask2.AddSuperPower;
+using CampTask2.PowerGenerator;
+using CampTask2.Warriors;
 
 namespace CampTask2
 {
@@ -6,7 +9,22 @@ namespace CampTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var user1 = new Orc();
+            var user2 = new Archer();
+
+            var generator = new SuperPowerGenerator();
+            var generatedPower = generator.GetSuperPower();
+
+            ISuperPower hp = new AddHp();
+            ISuperPower power = new AddPower();
+
+            user1.AddSuperPower(hp, generatedPower);
+            user2.AddSuperPower(power, generatedPower);
+
+            BattleScene.Fight(user1, user2);
+
+
+            Console.ReadKey();
         }
     }
 }
